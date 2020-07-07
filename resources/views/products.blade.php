ec571bbf4d909a60cb917c255b49cd06a0d892da
Edoardo Piergentili <epiergentili@outlook.it>
2020-07-07 20:05:30 +0200

    @4END?

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -5,5 +5,33 @@
 {{-- CONTENT --}}
 
 @section('content')
-
+  <div class="container">
+    <div class="row">
+      <h1 class="mt-5 mb-5">M%B - Products</h1>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>NAME</th>
+            <th>CATEGORY</th>
+            <th>TYPE</th>
+            <th>SELLER</th>
+            <th>PRICE</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach ($products as $product)
+            <tr>
+              <td>{{ $product->id }}</td>
+              <td>{{ $product->name }}</td>
+              <td>{{ $product->category }}</td>
+              <td>{{ $product->type }}</td>
+              <td>{{ $product->seller }}</td>
+              <td>{{ $product->price }}</td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
+  </div>
 @endsection
